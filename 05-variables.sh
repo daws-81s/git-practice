92ea76fd9ebfcb9a5db860d93c318f750f5850e9
sivakumarreddym <info@joindevops.com>
2024-08-23 08:31:36 +0530

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,11 @@
 
 echo "Please enter your username::"
 
-read USERNAME
+read -s USERNAME #takes input into USERNAME variable
 
 echo "Username entered is: $USERNAME"
+
+echo "Please enter your password::"
+
+read -s PASSWORD
+echo "Password entered is: $PASSWORD"
