c866789345ad1e6439acd4ca9ae24dfe5d569209
sivakumarreddym <info@joindevops.com>
2024-08-23 08:13:04 +0530

    added conversation script

---
+++ 02-converstation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh. I am fine. How are you doing?"
+echo "Ramesh:: I am doing good Suresh. What's going on?"
+echo "Suresh:: I started learning Shell Script Ramesh"
