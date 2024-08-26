687341e13a8751d9c4b988cb48a6576e199d438e
sivakumarreddym <info@joindevops.com>
2024-08-26 08:10:22 +0530

    functions

--- 11-installation.sh
+++ 11-installation.sh
@@ -9,5 +9,13 @@ then
     exit 1
 fi
 
-dnf install git -y
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+else
+    echo "Git is already installed, nothing to do.."
+fi
 
