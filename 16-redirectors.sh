c5b8c96c02399b752ec74be84e12696b84c74960
sivakumarreddym <info@joindevops.com>
2024-08-27 08:10:49 +0530

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -23,10 +23,10 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE | tee -a $LOG_FILE
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$2 is... $G SUCCESS $N" &>>$LOG_FILE | tee -a $LOG_FILE
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
