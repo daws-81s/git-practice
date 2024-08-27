2c68a40c2411c9da809150748e313396c4828ff7
sivakumarreddym <info@joindevops.com>
2024-08-27 07:31:15 +0530

    shell

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-echo "1"
-echo "2"
-echo "3"
-echo "4"
-echo "5"
-echo "6"
-echo "7"
-echo "8"
-echo "9"
-echo "10"
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+	echo $i
+done
+
+# echo "1"
+# echo "2"
+# echo "3"
+# echo "4"
+# echo "5"
+# echo "6"
+# echo "7"
+# echo "8"
+# echo "9"
+# echo "10"
