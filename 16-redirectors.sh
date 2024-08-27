ff94305f9d00b97b4e8de1c7292d91391549b6ff
sivakumarreddym <info@joindevops.com>
2024-08-27 08:08:38 +0530

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -15,7 +15,7 @@ Y="\e[33m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please run this script with root priveleges $N" &>>$LOG_FILE
+        echo -e "$R Please run this script with root priveleges $N" | tee -a &>>$LOG_FILE
         exit 1
     fi
 }
@@ -23,10 +23,10 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
+        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE | tee -a &>>$LOG_FILE
         exit 1
     else
-        echo -e "$2 is... $G SUCCESS $N" &>>$LOG_FILE
+        echo -e "$2 is... $G SUCCESS $N" &>>$LOG_FILE | tee -a &>>$LOG_FILE
     fi
 }
 
@@ -35,7 +35,7 @@ USAGE(){
     exit 1
 }
 
-echo "Script started executing at: $(date)" &>>$LOG_FILE
+echo "Script started executing at: $(date)" | tee -a &>>$LOG_FILE
 
 CHECK_ROOT
 
@@ -49,10 +49,10 @@ do
     dnf list installed $package &>>$LOG_FILE
     if [ $? -ne 0 ]
     then
-        echo "$package is not installed, going to install it.." &>>$LOG_FILE
+        echo "$package is not installed, going to install it.." | tee -a &>>$LOG_FILE
         dnf install $package -y &>>$LOG_FILE
         VALIDATE $? "Installing $package"
     else
-        echo -e "$package is already $Y installed..nothing to do $N" &>>$LOG_FILE
+        echo -e "$package is already $Y installed..nothing to do $N" | tee -a &>>$LOG_FILE
     fi
 done
