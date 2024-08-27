693dcc8676134f47718202939ac97e9c5f973c21
sivakumarreddym <info@joindevops.com>
2024-08-27 07:58:45 +0530

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -10,6 +10,7 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
@@ -30,3 +31,16 @@ VALIDATE(){
 }
 
 CHECK_ROOT
+
+for package in $@ # $@ refers to all arguments passed to it
+do
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then
+        echo "$package is not installed, going to install it.." &>>$LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "Installing $package"
+    else
+        echo "$package is already $Y installed..nothing to do $N" &>>$LOG_FILE
+    fi
+done
