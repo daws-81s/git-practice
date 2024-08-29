00e9d5de22a3ca01298e9699741029407bafb93f
sivakumarreddym <info@joindevops.com>
2024-08-29 08:32:31 +0530

    shell

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -6,5 +6,10 @@ DISK_THRESHOLD=5 #real projects, it is usually 75
 
 while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
-    echo $line
+    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        echo "$PARTITION is more than $DISK_THRESHOLD, current value: $USAGE. Please check"
+    fi
 done <<< $DISK_USAGE
